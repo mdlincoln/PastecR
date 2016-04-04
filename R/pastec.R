ca76cc8cd8bedf869aa1266f5858340d58a86843
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-04-04 12:41:25 -0400

    create a package-level documentation file

---
+++ R/pastec.R
@@ -0,0 +1,6 @@
+#' PastecR: Wrapper for the Pastec Image Search and Recognition Engine API
+#'
+#' The PastecR package offers methods for interacting with the Pastec server API, as well as helper methods for interpreting the image search results in an R-friendly way.
+#'
+#' @name PastecR
+NULL
