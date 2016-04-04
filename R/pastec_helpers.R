4223c2836d23fee762358b28f4f184d9b21c4f5b
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-04-04 12:36:26 -0400

    Mask the helper functions from roxygen

--- R/pastec_helpers.R
+++ R/pastec_helpers.R
@@ -1,9 +1,9 @@
-#' Helper function to read JSON response from
+# Helper function to read JSON response from
 jsonify <- function(httr_response) {
   jsonlite::fromJSON(httr::content(httr_response, as = "text", encoding = "UTF-8"))
 }
 
-#' Check the pastec server ping response
+# Check the pastec server ping response
 check_pastec_server <- function(server = pastec_server()) {
   ping_response <- httr::POST(url = server, body = '{"type":"PING"}')
   stopifnot(jsonify(ping_response)$type == "PONG")
