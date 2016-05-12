4553d61af455d6e8ffbc76a34d6ae79ccb9eb978
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-05-12 14:27:21 -0400

    unused method

--- R/pastec_helpers.R
+++ R/pastec_helpers.R
@@ -3,12 +3,6 @@ jsonify <- function(httr_response) {
   jsonlite::fromJSON(httr::content(httr_response, as = "text", encoding = "UTF-8"))
 }
 
-# Check the pastec server ping response
-check_pastec_server <- function(server = pastec_server()) {
-  ping_response <- httr::POST(url = server, body = '{"type":"PING"}')
-  stopifnot(jsonify(ping_response)$type == "PONG")
-}
-
 # Check if submitted IDs are whole numbers
 is.wholenumber <- function(x, tol = .Machine$double.eps^0.5) {
   abs(x - round(x)) < tol
