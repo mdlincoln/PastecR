4c9b4dccda6f76c1d342ddba6316ee51ec71e3d4
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-05-12 16:53:03 -0400

    A few links and details for _pastec_server funs

--- R/pastec_servers.R
+++ R/pastec_servers.R
@@ -3,6 +3,10 @@
 #' @param url Defaults to \code{localhost://}
 #' @param port Pastec default port is 4212
 #'
+#' @return A list with the attributes of the server connection
+#'
+#' @seealso \link{hosted_pastec_server}
+#'
 #' @export
 open_pastec_server <- function(url = "localhost", port = 4212) {
   list(type = "open", url = url, port = port)
@@ -16,8 +20,17 @@ open_pastec_server_url <- function(pastec_server) {
 
 #' Link to a hosted Pastec server
 #'
-#' @param index_id Character. ID string of the index hosted at \url{https://api.pastec.io}.
-#' @param auth_key Character. Authentication key string for your account at \url{https://api.pastec.io}.
+#' Your \code{index_id} and \code{auth_key} can be found at
+#' \url{https://api.pastec.io/manager/apiPage} once you make an account.
+#'
+#' @param index_id Character. ID string of the index hosted at
+#'   \url{https://api.pastec.io}.
+#' @param auth_key Character. Authentication key string for your account at
+#'   \url{https://api.pastec.io}.
+#'
+#' @return A list with the attributes of the server connection
+#'
+#' @seealso \likn{open_pastec_server}
 #'
 #' @export
 hosted_pastec_server <- function(index_id, auth_key) {
