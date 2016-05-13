e71f56d1232a7578c2b7def66e5ac1aa25c98463
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-05-12 23:09:52 -0400

    Also skip vignette chunks on Travis and AV
    At least until I can fix #2

--- vignettes/pastec_api.R
+++ vignettes/pastec_api.R
@@ -1,5 +1,8 @@
 ## ----setup, include=FALSE------------------------------------------------
 NOT_CRAN <- identical(tolower(Sys.getenv("NOT_CRAN")), "true")
+NOT_TRAVIS <- !identical(Sys.getenv("TRAVIS"), "true")
+NOT_APPVEYOR <- !identical(Sys.getenv("APPVEYOR"), "True")
+RUN_CHUNK <- all(NOT_CRAN, NOT_TRAVIS, NOT_APPVEYOR)
 
 ## ----pastec_server-------------------------------------------------------
 library(PastecR)
@@ -11,7 +14,7 @@ ops <- open_pastec_server(url = "localhost", port = 4212)
 # <https://api.pastec.io/manager/apiPage>
 hps <- hosted_pastec_server(index_id = "XXXXXXXXXXXXXXXXXXXX", auth_key = "XXXXXXXXXXXXXXXXXXXX")
 
-## ----add_image, eval=NOT_CRAN, purl=NOT_CRAN-----------------------------
+## ----add_image, eval=RUN_CHUNK, purl=RUN_CHUNK---------------------------
 # Get image paths
 erasmus1 <- system.file("img", "RP-P-1906-1485.jpg", package = "PastecR")
 erasmus2 <- system.file("img", "RP-P-1906-1486.jpg", package = "PastecR")
@@ -26,7 +29,7 @@ add_image(nightwatch, 4, ops)
 # Images can also be removed by index
 remove_image(4, ops)
 
-## ----search_image, eval=NOT_CRAN, purl=NOT_CRAN--------------------------
+## ----search_image, eval=RUN_CHUNK, purl=RUN_CHUNK------------------------
 search_result <- search_image(erasmus1, ops)
 results_as_data_frame(search_result)
 
