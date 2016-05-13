7ab7e03d996d21fcd02ea178d12ccf155572a685
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-05-12 20:51:07 -0400

    Build with a NOT_CRAN variable
    This is using Jenny Bryan's method for building API wrappers for CRAN submission

--- vignettes/pastec_api.R
+++ vignettes/pastec_api.R
@@ -1,3 +1,6 @@
+## ----setup, include=FALSE------------------------------------------------
+NOT_CRAN <- identical(tolower(Sys.getenv("NOT_CRAN")), "true")
+
 ## ----pastec_server-------------------------------------------------------
 library(PastecR)
 
@@ -7,4 +10,23 @@ ops <- open_pastec_server(url = "localhost", port = 4212)
 # You can find the index_id and auth_key for your api.pastec.io account at
 # <https://api.pastec.io/manager/apiPage>
 hps <- hosted_pastec_server(index_id = "XXXXXXXXXXXXXXXXXXXX", auth_key = "XXXXXXXXXXXXXXXXXXXX")
+
+## ----add_image, eval=NOT_CRAN, purl=NOT_CRAN-----------------------------
+# Get image paths
+erasmus1 <- system.file("img", "RP-P-1906-1485.jpg", package = "PastecR")
+erasmus2 <- system.file("img", "RP-P-1906-1486.jpg", package = "PastecR")
+erasmus3 <- system.file("img", "RP-P-1906-1487.jpg", package = "PastecR")
+nightwatch <- system.file("img", "SK-C-5.jpg", package = "PastecR")
+
+add_image(erasmus1, 1, ops)
+add_image(erasmus2, 2, ops)
+add_image(erasmus3, 3, ops)
+add_image(nightwatch, 4, ops)
+
+# Images can also be removed by index
+remove_image(4, ops)
+
+## ----search_image, eval=NOT_CRAN, purl=NOT_CRAN--------------------------
+search_result <- search_image(erasmus1, ops)
+results_as_data_frame(search_result)
 
